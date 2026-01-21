65d74b2e0e3e02bee27fc7224e2133c9a2b3431a
Makenna-Jolley <makennajjolley@gmail.com>
2026-01-21 15:39:01 -0600

    Added test

--- test_git_lab.py
+++ test_git_lab.py
@@ -49,9 +49,11 @@ class TestUtils(unittest.TestCase):
     
     def test_hello(self):
         self.assertTrue(2)
+        
     def test_multiply_numbers(self): 
         self.assertEqual(multiply_numbers(2, 3), 6) 
         self.assertEqual(multiply_numbers(-1, 1), -1)
+        self.assertNotEqual(multiply_numbers(1, 1), -1)
 
 if __name__ == '__main__':
     unittest.main()
